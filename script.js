6620d7945473b876ffb5515ef831ee3c8d481183
Gaurav kumar <ranasanga12.156@gmail.com>
2026-03-30 23:39:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+// Jab 'Sign Up' button click ho, tab slide karke registration dikhao
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+// Jab 'Sign In' button click ho, tab wapas login form dikhao
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
